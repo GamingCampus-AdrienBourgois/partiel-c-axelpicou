6a78a0a46a8a0099bafa2189b7f84710f20a9c7c
axel picou <axel.picou@gmail.com>
2024-01-19 14:08:47 +0100

    exo 1 finito

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -9,14 +9,33 @@ float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, Temper
 {
 	if (_from == _to)
 	{
+		float _result = _value;
 		std::cout <<"the units are the same" << std::endl;
-		return -1.0f;
+		return _result;
 	}
 	else if (_from == TemperatureUnits::KELVIN && _to == TemperatureUnits::CELSIUS)
 	{
 		float _result = _value - 273.15;
 		std::cout << "en kelvin :"<< _value<< " apres convertion en celsius :" << _result << std::endl;
-		return -1.0f;
+		return _result;
+	}
+	else if (_from == TemperatureUnits::KELVIN && _to == TemperatureUnits::FAHRENHEIT)
+	{
+		float _result = (9.0 / 5) * (_value - 273.15) + 32;
+		std::cout << "en kelvin :" << _value << " apres convertion en fahrenheit :" << _result << std::endl;
+		return _result;
+	}
+	else if (_from == TemperatureUnits::CELSIUS && _to == TemperatureUnits::KELVIN)
+	{
+		float _result = _value + 273.15;
+		std::cout << "en celsius :" << _value << " apres convertion en kelvin :" << _result << std::endl;
+		return _result;
+	}
+	else if (_from == TemperatureUnits::FAHRENHEIT && _to == TemperatureUnits::CELSIUS)
+	{
+		float _result = (5 / 9.0) * (_value - 32);
+		std::cout << "en fahrenheit :" << _value << " apres convertion en celsius :" << _result << std::endl;
+		return _result;
 	}
 
 
